ea6d167cd5e77dc15bda7719624496b4cb9b453d
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 17:33:39 +0900

    Stream 에서 forEach 에 대한 설명 추가

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -17,6 +17,13 @@ public class Main {
         Stream<Integer> stream = list.stream();
         // forEach() 메소드를 이용한 스트림 요소의 순차 접근
         stream.forEach(System.out::println);
+        /*
+            forEach:
+
+            Stream 클래스의 forEach() 메소드는 해당 스트림의 요소를 하나씩 소모해가며 순차적으로 요소에 접근하는 메소드입니다.
+            따라서 같은 스트림으로는 forEach() 메소드를 한 번밖에 호출할 수 없습니다.
+            단, 원본 데이터의 요소를 소모하는 것은 아니므로, 같은 데이터에서 또 다른 스트림을 생성하여 forEach() 메소드를 호출하는 것은 가능합니다.
+         */
     }
 
 
