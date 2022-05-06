3d072e8a06ff32fb6dd0305fabaed806c8d9b4de
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 18:00:10 +0900

    스트림의 생성 - 람다 표현식

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,15 +1,17 @@
 package stream;
 
-import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        // 특정 타입의 난수로 이루어진 스트림 생성
-        IntStream stream = new Random().ints(4);
-        // IntStream stream = new Random().ints().limit(4);
-        stream.forEach(System.out::println);
+        // iterate() 메소드는 시드(seed)로 명시된 값을 람다 표현식에 사용하여 반환된 값을 다시 시드로 사용하는 방식으로 무한 스트림을 생성합니다.
+        IntStream stream1 = IntStream.iterate(1, n -> n + 2).limit(50);// 2, 4, 6, 8, 10, 12, ...
+        stream1.forEach(System.out::println);
+
+        // 반면에 generate() 메소드는 매개변수가 없는 람다 표현식을 사용하여 반환된 값으로 무한 스트림을 생성합니다.
+        IntStream stream2 = IntStream.generate(() -> 1).limit(100);
+        stream2.forEach(System.out::println);
     }
 
 }
