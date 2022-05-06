f2b264196e4db66bef84e53e3351cd8fad6a3622
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 17:37:21 +0900

    스트림의 생성 - 배열

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,29 +1,21 @@
 package stream;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        String[] arr = {"넷", "둘", "셋", "하나"};
 
-        list.add(4);
-        list.add(2);
-        list.add(3);
-        list.add(1);
+        // 배열에서 스트림 생성
+        Stream<String> stream1 = Arrays.stream(arr);
+        stream1.forEach(e -> System.out.print(e + " "));
+        System.out.println();
 
-        // 컬렉션에서 스트림 생성
-        Stream<Integer> stream = list.stream();
-        // forEach() 메소드를 이용한 스트림 요소의 순차 접근
-        stream.forEach(System.out::println);
-        /*
-            forEach:
-
-            Stream 클래스의 forEach() 메소드는 해당 스트림의 요소를 하나씩 소모해가며 순차적으로 요소에 접근하는 메소드입니다.
-            따라서 같은 스트림으로는 forEach() 메소드를 한 번밖에 호출할 수 없습니다.
-            단, 원본 데이터의 요소를 소모하는 것은 아니므로, 같은 데이터에서 또 다른 스트림을 생성하여 forEach() 메소드를 호출하는 것은 가능합니다.
-         */
+        // 배열의 특정 부분만을 이용한 스트림 생성
+        Stream<String> stream2 = Arrays.stream(arr, 1, 3);
+        stream2.forEach(e -> System.out.print(e + " "));
     }
 
 
