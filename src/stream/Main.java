91dca97b1f10c79f7db2477bbf1dccf1e55f6b11
powerhorang2 <tjrdbqls3@naver.com>
2022-05-09 12:13:39 +0900

    스트림의 중개 연산 - 스트림 제한

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,25 +1,25 @@
 package stream;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         /*
-        map() 메소드는 해당 스트림의 요소들을 주어진 함수에 인수로 전달하여, 그 반환값들로 이루어진 새로운 스트림을 반환합니다.
-        만약 해당 스트림의 요소가 배열이라면, flatMap() 메소드를 사용하여 각 배열의 각 요소의 반환값을 하나로 합친 새로운 스트림을 얻을 수 있습니다.
-         */
+         limit() 메소드는 해당 스트림의 첫 번째 요소부터 전달된 개수만큼의 요소만으로 이루어진 새로운 스트림을 반환합니다.
+         skip() 메소드는 해당 스트림의 첫 번째 요소부터 전달된 개수만큼의 요소를 제외한 나머지 요소만으로 이루어진 새로운 스트림을 반환합니다.
+        */
+        IntStream stream1 = IntStream.range(0, 10);
+        IntStream stream2 = IntStream.range(0, 10);
+        IntStream stream3 = IntStream.range(0, 10);
 
-        //다음 예제는 문자열로 이루어진 스트림을 map() 메소드를 이용하여 각 문자열의 길이로 이루어진 스트림으로 변환하는 예제입니다.
-        Stream<String> stream = Stream.of("HTML", "CSS", "JAVA", "JAVASCRIPT");
-        stream.map(s -> s.length()).forEach(System.out::println);
+        stream1.skip(4).forEach(n -> System.out.print(n + " "));
+        System.out.println();
 
-        //다음 예제는 여러 문자열이 저장된 배열을 각 문자열에 포함된 단어로 이루어진 스트림으로 변환하는 예제입니다.
-        String[] arr = {"I study hard", "You study JAVA", "I am hungry"};
+        stream2.limit(5).forEach(n -> System.out.print(n + " "));
+        System.out.println();
 
-        Stream<String> stream1 = Arrays.stream(arr);
-        stream1.flatMap(s -> Stream.of(s.split(" +"))).forEach(System.out::println);
+        stream3.skip(3).limit(5).forEach(n -> System.out.print(n + " "));
     }
 
 }
