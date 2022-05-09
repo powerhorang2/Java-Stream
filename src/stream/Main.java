30b9428bf4ee6973d117fe5e3e5372106b4cadde
powerhorang2 <tjrdbqls3@naver.com>
2022-05-09 11:57:28 +0900

    스트림의 생성 - 빈 스트림

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,19 +1,13 @@
 package stream;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        /*
-        파일의 한 행(line)을 요소로 하는 스트림을 생성하기 위해 java.nio.file.Files 클래스에는 lines() 메소드가 정의되어 있습니다.
-        또한, java.io.BufferedReader 클래스의 lines() 메소드를 사용하면 파일뿐만 아니라 다른 입력으로부터도 데이터를 행(line) 단위로 읽어 올 수 있습니다.
-        */
-        String path = "어떤 파일의 경로 값";
-        Stream<String> stream = Files.lines(Paths.get(path));
+    public static void main(String[] args) {
+        //빈 스트림 생성
+        Stream<Object> stream = Stream.empty();
+        System.out.println(stream.count()); //스트림 요소의 총 개수를 출력함
     }
 
 }
