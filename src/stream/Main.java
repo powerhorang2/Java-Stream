069df3fbb8aec1c28961bc70d4f8fdffa552c22a
powerhorang2 <tjrdbqls3@naver.com>
2022-05-09 12:01:06 +0900

    스트림의 중개 연산 - 스트림 필터링

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,13 +1,19 @@
 package stream;
 
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        //빈 스트림 생성
-        Stream<Object> stream = Stream.empty();
-        System.out.println(stream.count()); //스트림 요소의 총 개수를 출력함
+        IntStream stream1 = IntStream.of(7, 5, 5, 2, 1, 2, 3, 5, 4, 6);
+        IntStream stream2 = IntStream.of(7, 5, 5, 2, 1, 2, 3, 5, 4, 6);
+
+        //스트림에서 중복된 요소를 제거함.
+        stream1.distinct().forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        //스트림에서 홀수만을 골라냄
+        stream2.filter(n -> n % 2 != 0).forEach(e -> System.out.print(e + " "));
     }
 
 }
