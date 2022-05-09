e286b9ffb51af64e39d93b22c2f5530c1d243321
powerhorang2 <tjrdbqls3@naver.com>
2022-05-09 17:56:10 +0900

    스트림의 중개 연산 - 스트림 연산 결과 확인

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,22 +1,20 @@
 package stream;
 
-import java.util.Comparator;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*
-          sorted() 메소드는 해당 스트림을 주어진 비교자(comparator)를 이용하여 정렬합니다.
-          이때 비교자를 전달하지 않으면 기본적으로 사전 편찬 순(natural order)으로 정렬하게 됩니다.
-         */
-        Stream<String> stream1 = Stream.of("JAVA", "HTML", "JAVASCRIPT", "CSS");
-        Stream<String> stream2 = Stream.of("JAVA", "HTML", "JAVASCRIPT", "CSS");
+        IntStream stream = IntStream.of(7, 5, 5, 2, 1, 2, 3, 5, 4, 6);
 
-        stream1.sorted().forEach(s -> System.out.print(s + " "));
-        System.out.println();
-
-        stream2.sorted(Comparator.reverseOrder()).forEach(s -> System.out.print(s + " "));
+        stream.peek(s -> System.out.println("원본 스트림: " + s))
+                .skip(2)
+                .peek(s -> System.out.println("skip(2) 실행 후: " + s))
+                .limit(5)
+                .peek(s -> System.out.println("limit(5) 실행 후: " + s))
+                .sorted()
+                .peek(s -> System.out.println("sorted() 실행 후: " + s))
+                .forEach(System.out::println);
     }
 
 }
