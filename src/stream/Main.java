7af69d5ef52c952463f2eb9edb77e688aee1b41c
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 15:30:21 +0900

    실수 - BigDecimal 사칙연산 : 더하기(add)

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -6,19 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 생성자 + 문자열로 초기화 하는 방법
-        BigDecimal value1 = new BigDecimal("12.23");
-        System.out.println(value1);
+        BigDecimal value = BigDecimal.valueOf(12.23);
 
-        // double 타입으로 초기화하는 방법
-        // 내부적으로 생성자 + 문자열을 사용한다.
-        BigDecimal value2 = BigDecimal.valueOf(34.45);
-        System.out.println(value2);
-
-        // 아래와 같이 사용하면 안 된다.
-        // 12.230000000000000426325641456060111522674560546875
-        BigDecimal dontDoThis = new BigDecimal(12.23);
-        System.out.println(dontDoThis);
+        // 13.23
+        BigDecimal returnValue = value.add(BigDecimal.ONE);
+        System.out.println(returnValue);
     }
 
 }
