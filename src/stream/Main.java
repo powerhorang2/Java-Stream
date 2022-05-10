ab094e204fa711f4be78440c7bd468d8814b83b5
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 11:28:56 +0900

    스트림의 최종 연산 - 요소의 통계 : count(), min(), max()

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -6,16 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         /*
-        1. anyMatch() : 해당 스트림의 일부 요소가 특정 조건을 만족할 경우에 true 를 반환함.
-        2. allMatch() : 해당 스트림의 모든 요소가 특정 조건을 만족할 경우에 true 를 반환함.
-        3. noneMatch() : 해당 스트림의 모든 요소가 특정 조건을 만족하지 않을 경우에 true 를 반환함.
-        세 메소드 모두 인수로 Predicate 객체를 전달받으며, 요소의 검사 결과는 boolean 값으로 반환합니다.
+        count() 메소드는 해당 스트림의 요소의 총 개수를 long 타입의 값으로 반환합니다.
+        또한, max()와 min() 메소드를 사용하면 해당 스트림의 요소 중에서 가장 큰 값과 가장 작은 값을 가지는 요소를 참조하는 Optional 객체를 얻을 수 있습니다.
          */
         IntStream stream1 = IntStream.of(30, 90, 70, 10);
         IntStream stream2 = IntStream.of(30, 90, 70, 10);
 
-        System.out.println(stream1.anyMatch(n -> n > 80));
-        System.out.println(stream2.allMatch(n -> n > 80));
+        System.out.println(stream1.count());
+        System.out.println(stream2.max().getAsInt());
     }
 
 }
