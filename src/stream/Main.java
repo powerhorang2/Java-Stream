ea7a0ef5621525f1a56bddb4a6bfc1eab024b4c2
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 11:57:12 +0900

    Optional 클래스 - Optional 객체에 접근

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -6,16 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         /*
-        of() 메소드나 ofNullable() 메소드를 사용하여 Optional 객체를 생성할 수 있습니다.
-
-        of() 메소드는 null 이 아닌 명시된 값을 가지는 Optional 객체를 반환합니다.
-        만약 of() 메소드를 통해 생성된 Optional 객체에 null 이 저장되면 NullPointerException 예외가 발생합니다.
-
-        따라서 만약 참조 변수의 값이 만에 하나 null 이 될 가능성이 있다면, ofNullable() 메소드를 사용하여 Optional 객체를 생성하는 것이 좋습니다.
-        ofNullable() 메소드는 명시된 값이 null 이 아니면 명시된 값을 가지는 Optional 객체를 반환하며, 명시된 값이 null 이면 비어있는 Optional 객체를 반환합니다.
+        get() 메소드를 사용하면 Optional 객체에 저장된 값에 접근할 수 있습니다.
+        만약 Optional 객체에 저장된 값이 null 이면, NoSuchElementException 예외가 발생합니다.
+        따라서 get() 메소드를 호출하기 전에 isPresent() 메소드를 사용하여 Optional 객체에 저장된 값이 null 인지 아닌지를 먼저 확인한 후 호출하는 것이 좋습니다.
          */
         Optional<String> opt = Optional.ofNullable("자바 Optional 객체");
-        System.out.println(opt.get());
+
+        if(opt.isPresent()) {
+            System.out.println(opt.get());
+        }
+
+        /*
+        또한, 다음과 같은 메소드를 사용하여 null 대신에 대체할 값을 지정할 수도 있습니다.
+
+        1. orElse() 메소드 : 저장된 값이 존재하면 그 값을 반환하고, 값이 존재하지 않으면 인수로 전달된 값을 반환함.
+        2. orElseGet() 메소드 : 저장된 값이 존재하면 그 값을 반환하고, 값이 존재하지 않으면 인수로 전달된 람다 표현식의 결괏값을 반환함.
+        3. orElseThrow() 메소드 : 저장된 값이 존재하면 그 값을 반환하고, 값이 존재하지 않으면 인수로 전달된 예외를 발생시킴.
+         */
+
+        Optional<Object> emptyOpt = Optional.empty(); // Optional 를 null 로 초기화함.
+
+        System.out.println(emptyOpt.orElse("빈 Optional 객체"));
+        System.out.println(emptyOpt.orElseGet(String::new));
     }
 
 }
