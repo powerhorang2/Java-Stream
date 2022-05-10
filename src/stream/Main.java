135e24f4a0a8ae7075c8c8874e88d7be6575a030
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 15:42:28 +0900

    실수 - BigDecimal 사칙연산 : 나누기(divide) 예외

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BigDecimal value1 = BigDecimal.valueOf(0.12);
+        BigDecimal value1 = BigDecimal.valueOf(0.13);
         BigDecimal value2 = BigDecimal.valueOf(0.3);
 
+        // 정확하게 나누어 몫이 떨어지지 않는 수의 경우 ArithmeticException 예외를 던진다.
         BigDecimal divideValue = value1.divide(value2);
         System.out.println(divideValue);
     }
