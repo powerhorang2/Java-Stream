00e84bc98f0c95956784f324ee95a9ccc080e1c6
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 11:30:43 +0900

    스트림의 최종 연산 - 요소의 연산 : sum(), average()

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,19 +1,21 @@
 package stream;
 
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         /*
-        count() 메소드는 해당 스트림의 요소의 총 개수를 long 타입의 값으로 반환합니다.
-        또한, max()와 min() 메소드를 사용하면 해당 스트림의 요소 중에서 가장 큰 값과 가장 작은 값을 가지는 요소를 참조하는 Optional 객체를 얻을 수 있습니다.
+        IntStream 이나 DoubleStream 과 같은 기본 타입 스트림에는
+        해당 스트림의 모든 요소에 대해 합과 평균을 구할 수 있는 sum()과 average() 메소드가 각각 정의되어 있습니다.
+        이때 average() 메소드는 각 기본 타입으로 래핑 된 Optional 객체를 반환합니다.
          */
         IntStream stream1 = IntStream.of(30, 90, 70, 10);
-        IntStream stream2 = IntStream.of(30, 90, 70, 10);
+        DoubleStream stream2 = DoubleStream.of(30.3, 90.9, 70.7, 10.1);
 
-        System.out.println(stream1.count());
-        System.out.println(stream2.max().getAsInt());
+        System.out.println(stream1.sum());
+        System.out.println(stream2.average().getAsDouble());
     }
 
 }
