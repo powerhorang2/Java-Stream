2b30a8298244f01349f5dd0664afcd7581af13e7
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 10:34:20 +0900

    스트림의 최종 연산 - 요소의 출력

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,20 +1,13 @@
 package stream;
 
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        IntStream stream = IntStream.of(7, 5, 5, 2, 1, 2, 3, 5, 4, 6);
 
-        stream.peek(s -> System.out.println("원본 스트림: " + s))
-                .skip(2)
-                .peek(s -> System.out.println("skip(2) 실행 후: " + s))
-                .limit(5)
-                .peek(s -> System.out.println("limit(5) 실행 후: " + s))
-                .sorted()
-                .peek(s -> System.out.println("sorted() 실행 후: " + s))
-                .forEach(System.out::println);
+        Stream<String> stream = Stream.of("넷", "둘", "셋", "하나");
+        stream.forEach(System.out::println);
     }
 
 }
