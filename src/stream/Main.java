e255dfa38db5b02eda532fe427540d7e24c9dbdc
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 15:25:05 +0900

    실수 - 부동 소수점 방식의 오차 해결책 BigDecimal

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,14 +1,24 @@
 package stream;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-        double value1 = 12.23;
-        double value2 = 34.45;
 
-        // 46.68 ??
-        System.out.println(value1 + value2);
-        // 46.68000000000001
+        // 생성자 + 문자열로 초기화 하는 방법
+        BigDecimal value1 = new BigDecimal("12.23");
+        System.out.println(value1);
+
+        // double 타입으로 초기화하는 방법
+        // 내부적으로 생성자 + 문자열을 사용한다.
+        BigDecimal value2 = BigDecimal.valueOf(34.45);
+        System.out.println(value2);
+
+        // 아래와 같이 사용하면 안 된다.
+        // 12.230000000000000426325641456060111522674560546875
+        BigDecimal dontDoThis = new BigDecimal(12.23);
+        System.out.println(dontDoThis);
     }
 
 }
