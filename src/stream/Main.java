c5d20a4754895c9e0ab1daa6606cd3606fb120ef
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 16:05:47 +0900

    실수 - BigDecimal 소수점 처리 방식

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,22 +1,36 @@
 package stream;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
 
     public static void main(String[] args) {
 
         /*
-        최댓값은 max, 최솟값은 min 메서드를 사용하면 전달되는 파라미터와 비교하여 구할 수 있다.
+        자바에서는 BigDecimal 클래스에 다양한 소수점 처리 방식을 제공하고 있다.
+        몇 가지 예시로 살펴보면 아래와 같다.
          */
 
-        BigDecimal value = BigDecimal.valueOf(10);
+        // 소수점 첫 번째까지 표현, 두번째 자리에서 반올림
+        // 12.4
+        BigDecimal value1 = BigDecimal.valueOf(12.35).setScale(1, RoundingMode.HALF_UP);
+        System.out.println("value1 = " + value1);
 
-        // 10
-        System.out.println(value.max(BigDecimal.ONE));
+        // 소수점 이하 모두 제거하고 올림
+        // 13
+        BigDecimal value2 = BigDecimal.valueOf(12.34).setScale(0, RoundingMode.CEILING);
+        System.out.println("value2 = " + value2);
 
-        // 1
-        System.out.println(value.min(BigDecimal.ONE));
+        // 음수인 경우는 특정 자릿수 이하 제거
+        // -12.3
+        BigDecimal value3 = BigDecimal.valueOf(-12.34).setScale(1, RoundingMode.CEILING);
+        System.out.println("value3 = " + value3);
+
+        // 특정 자릿수 이하 버림
+        // 12.3
+        BigDecimal value4 = new BigDecimal("12.37").setScale(1, RoundingMode.FLOOR);
+        System.out.println("value4 = " + value4);
     }
 
 }
