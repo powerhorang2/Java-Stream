eb5ba2d4be216b71e3e152f66bba0e64fbffca06
powerhorang2 <tjrdbqls3@naver.com>
2022-05-10 11:24:25 +0900

    스트림의 최종 연산 - 요소의 검색

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -1,30 +1,27 @@
 package stream;
 
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Stream<String> stream1 = Stream.of("넷", "둘", "셋", "하나");
-        Stream<String> stream2 = Stream.of("넷", "둘", "셋", "하나");
-
         /*
-        스트림의 최종 연산은 모두 스트림의 각 요소를 소모하여 연산을 수행하게 됩니다.
-        하지만 reduce() 메소드는 첫 번째와 두 번째 요소를 가지고 연산을 수행한 뒤, 그 결과와 세 번째 요소를 가지고 또다시 연산을 수행합니다.
-        이런 식으로 해당 스트림의 모든 요소를 소모하여 연산을 수행하고, 그 결과를 반환하게 됩니다.
+        findFirst()와 findAny() 메소드는 해당 스트림에서 첫 번째 요소를 참조하는 Optional 객체를 반환합니다.
+        두 메소드 모두 비어 있는 스트림에서는 비어있는 Optional 객체를 반환합니다.
          */
-        Optional<String> result1 = stream1.reduce((s1, s2) -> s1 + "++" + s2);
-        result1.ifPresent(System.out::println);
+        IntStream stream1 = IntStream.of(4, 2, 7, 3, 5, 1, 6);
+        IntStream stream2 = IntStream.of(4, 2, 7, 3, 5, 1, 6);
 
+        OptionalInt result1 = stream1.sorted().findFirst();
+        System.out.println(result1.getAsInt());
+
+        OptionalInt result2 = stream2.sorted().findAny();
+        System.out.println(result2.getAsInt());
         /*
-        또한, 인수로 초깃값을 전달하면 초깃값과 해당 스트림의 첫 번째 요소와 연산을 시작하며, 그 결과와 두 번째 요소를 가지고 계속해서 연산을 수행하게 됩니다.
-        인수로 초깃값을 전달하는 reduce() 메소드의 반환 타입은 Optional<T>가 아닌 T 타입입니다.
-        그 이유는 비어 있는 스트림과 reduce 연산을 할 경우 전달받은 초깃값을 그대로 반환해야 하기 때문입니다.
+        위의 예제에서 볼 수 있듯이 두 메소드의 결과는 같게 출력됩니다.
+        하지만 병렬 스트림인 경우에는 findAny() 메소드를 사용해야만 정확한 연산 결과를 반환할 수 있습니다.
          */
-        String result2 = stream2.reduce("시작", (s1, s2) -> s1 + "++" + s2);
-        System.out.println(result2);
     }
 
 }
